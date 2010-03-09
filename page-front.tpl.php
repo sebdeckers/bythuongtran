fbcac9d36f05495bed7fbca14ecf7bacdf593b80
Sebastiaan Deckers <cbas@pandion.be>
2010-03-10 00:01:07 +0100

    - Removed #media * CSS that is no longer used - Changed primary menu font, size - Fixed front page introduction font (no more image replacement) - Added colour to copyright name - Added favicon - HTML tidy

--- page-front.tpl.php
+++ page-front.tpl.php
@@ -1,100 +1,83 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
-  <head>
-    <?php print $head ?>
-    <title><?php print $head_title ?></title>
-	<?php
-		drupal_add_css(path_to_theme() . '/styles/galleria.css', 'theme');
-		drupal_add_css(path_to_theme() . '/styles/slideshow.css', 'theme');
-		$styles = $variables['styles'] = drupal_get_css();
-		drupal_add_js(path_to_theme() . '/scripts/jquery.galleria.js', 'theme');
-		drupal_add_js(path_to_theme() . '/scripts/slideshow.js', 'theme');
-		$scripts = $variables['script'] = drupal_get_js();
-	?>
-	<?php print $styles ?>
-    <?php print $scripts ?>
-  </head>
-  <body>	
-	<div id="frontpage">
-	  <div id="container">
-	    <div id="header">
-		  <div id="language-switcher">
-		    <?php print $languageswitcher; ?>
-		  </div>
-		  <h1><span></span><?php print $site_name; ?></h1>
-				
-		  <div class="navigation">
-            <?php if (isset($primary_links)) : ?>
-              <?php print theme('links', $primary_links, array('class' => 'primary-links')) ?>
-            <?php endif; ?>
-          </div>		
-	    </div>
-	    <div id="content" class="clearfix">
-		
-		<div id="media">
-
-			<!-- Wrapper where the main image is displayed. Can be used to style a border etc. -->
-			<div class="slideshow-big">
-
-				<!-- Fallback for disabled javascript -->
-				<noscript>
-					<img src="http://farm5.static.flickr.com/4003/4419893170_de1f79eb6c.jpg" />
-				</noscript>
-
+<head>
+<?php print $head ?>
+<title><?php print $head_title ?></title>
+<?php
+drupal_add_css(path_to_theme() . '/styles/galleria.css', 'theme');
+drupal_add_css(path_to_theme() . '/styles/slideshow.css', 'theme');
+$styles = $variables['styles'] = drupal_get_css();
+drupal_add_js(path_to_theme() . '/scripts/jquery.galleria.js', 'theme');
+drupal_add_js(path_to_theme() . '/scripts/slideshow.js', 'theme');
+$scripts = $variables['script'] = drupal_get_js();
+print $styles;
+print $scripts;
+?>
+</head>
+<body>
+<div id="frontpage">
+	<div id="container">
+		<div id="header">
+			<div id="language-switcher">
+				<?php print $languageswitcher; ?>
 			</div>
-
-			<div class="slideshow-controls">
-
-				<!-- List of thumbnail IMGs with Anchors to the big images. -->
-				<a href="#" class="slideshow-back" title="Back">Back</a>
-				<ul class="slideshow-list"> </ul>
-				<a href="#" class="slideshow-forward" title="Forward">Forward</a>
-
-				<!-- Buttons to open the corresponding full screen Flickr slideshow and main gallery. -->
-				<a class="slideshow-fullscreen" target="_blank" href="http://www.flickr.com/photos/bythuongtran/sets/72157623394214280/show/" title="Full screen">Full screen</a>
-				<a class="slideshow-more" target="_blank" href="http://www.flickr.com/photos/bythuongtran" title="More photos">More photos</a>
-
-			</div>
-
+			<h1><span></span><?php print $site_name; ?></h1>
+			<div class="navigation">
+				<?php if (isset($primary_links)): print theme('links', $primary_links, array('class' => 'primary-links')); endif; ?>
+			</div>		
 		</div>
-
-          <div id="introduction">
-		    <p class="certified"><span></span>The certified wedding specialist in Vietnam and Singapore</p>
-
-              <?php if ($mission): print '<div id="mission">'. $mission .'</div>'; endif; ?>
-              <?php if ($tabs): print '<div id="tabs-wrapper" class="clear-block">'; endif; ?>
-              <?php //if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif; ?>
-              <?php if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif; ?>
-              <?php if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif; ?>
-              <?php if ($show_messages && $messages): print $messages; endif; ?>
-              <?php if ($help): print $help; endif; ?>
-              <?php print $content; ?>
-					
-			<p class="provision"><span></span>Providing premium wedding and destination wedding services inplanning and consulting</p>
-	      </div>
-	    </div>
-	    <div id="footer">
-		  <div id="sharing">
-			  <a class="a2a_dd" href="http://www.addtoany.com/share_save?linkname=Bythuongtran&amp;linkurl=bythuongtran.com">
-				<img src="/<?php print path_to_theme(); ?>/images/icon-addtoany.png" alt="Add to any" />
-			  </a>
-			  <script type="text/javascript">a2a_linkname="Bythuongtran";a2a_linkurl="bythuongtran.com";</script>
-			  <script type="text/javascript" src="http://static.addtoany.com/menu/page.js"></script>
-			  <p class="share">Share this website</p>
-              <p class="social">Follow us at:</p>
-	          <a href="http://www.facebook.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-facebook.png" alt="Facebook" /></a>
-	          <a href="http://twitter.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-twitter.png" alt="Twitter" /></a>
-	          <a href="http://blog.bythuongtran.com/"><img src="/<?php print path_to_theme(); ?>/images/icon-blogger.png" alt="Blogger" /></a>
-	          <a href="http://www.flickr.com/photos/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-flickr.png" alt="Flickr" /></a>
-		  </div>
-		  <div id="notice">
-            <?php if (isset($secondary_links)) : ?>
-              <?php print theme('links', $secondary_links, array('class' => 'secondary-links')) ?>
-            <?php endif; ?>
-            <p class="copyright">&copy; Bythongtran 2010. All rights reserved</p>
-          </div>
+		<div id="content" class="clearfix">
+			<div id="media">
+				<!-- Wrapper where the main image is displayed. Can be used to style a border etc. -->
+				<div class="slideshow-big">
+					<!-- Fallback for disabled javascript -->
+					<noscript>
+						<img src="http://farm5.static.flickr.com/4003/4419893170_de1f79eb6c.jpg" />
+					</noscript>
+				</div>
+				<div class="slideshow-controls">
+					<!-- List of thumbnail IMGs with Anchors to the big images. -->
+					<a href="#" class="slideshow-back" title="Back">Back</a>
+					<ul class="slideshow-list"> </ul>
+					<a href="#" class="slideshow-forward" title="Forward">Forward</a>
+					<!-- Buttons to open the corresponding full screen Flickr slideshow and main gallery. -->
+					<a class="slideshow-fullscreen" target="_blank" href="http://www.flickr.com/photos/bythuongtran/sets/72157623394214280/show/" title="Full screen">Full screen</a>
+					<a class="slideshow-more" target="_blank" href="http://www.flickr.com/photos/bythuongtran" title="More photos">More photos</a>
+				</div>
+			</div>
+			<div id="introduction">
+				<?php
+				if ($mission): print '<div id="mission">'. $mission .'</div>'; endif;
+				if ($tabs): print '<div id="tabs-wrapper" class="clear-block">'; endif;
+				//if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif;
+				if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif;
+				if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif;
+				if ($show_messages && $messages): print $messages; endif;
+				if ($help): print $help; endif;
+				print $content;
+				?>
+			</div>
+		</div>
+		<div id="footer">
+			<div id="sharing">
+				<a class="a2a_dd" href="http://www.addtoany.com/share_save?linkname=Bythuongtran&amp;linkurl=bythuongtran.com">
+					<img src="/<?php print path_to_theme(); ?>/images/icon-addtoany.png" alt="Add to any" />
+				</a>
+				<script type="text/javascript">a2a_linkname="Bythuongtran";a2a_linkurl="bythuongtran.com";</script>
+				<script type="text/javascript" src="http://static.addtoany.com/menu/page.js"></script>
+				<p class="share">Share this website</p>
+				<p class="social">Follow us at:</p>
+				<a href="http://www.facebook.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-facebook.png" alt="Facebook" /></a>
+				<a href="http://twitter.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-twitter.png" alt="Twitter" /></a>
+				<a href="http://blog.bythuongtran.com/"><img src="/<?php print path_to_theme(); ?>/images/icon-blogger.png" alt="Blogger" /></a>
+				<a href="http://www.flickr.com/photos/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-flickr.png" alt="Flickr" /></a>
+			</div>
+			<div id="notice">
+				<?php if (isset($secondary_links)): print theme('links', $secondary_links, array('class' => 'secondary-links')); endif; ?>
+				<p class="copyright">&copy; <span class="btt">by</span>thuongtran 2010. All rights reserved.</p>
+			</div>
 		</div> 
-	  </div>
-    </div>
-  </body>
+	</div>
+</div>
+</body>
 </html>
--- page.tpl.php
+++ page.tpl.php
@@ -1,72 +1,66 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
-  <head>
-    <?php print $head ?>
-    <title><?php print $head_title ?></title>
-    <?php print $styles ?>
-    <?php print $scripts ?>
-  </head>
-
-  <body>	
-	<div id="page">
-	  <div id="container">
-	    <div id="header">
-		  <div id="language-switcher">
-		    <?php print $languageswitcher; ?>
-		  </div>
-		  <h1><span></span><?php print $site_name; ?></h1>
-				
-		  <div class="navigation">
-            <?php if (isset($primary_links)) : ?>
-              <?php print theme('links', $primary_links, array('class' => 'primary-links')) ?>
-            <?php endif; ?>
-          </div>		
-	    </div>
-	    <div id="content" class="clearfix">
-		
-		  <div id="sidebar-left">
-			<?php print $sidebar_left; ?>
-			&nbsp;
-		  </div>
-		
-		  <div id="body">
-              <?php if ($mission): print '<div id="mission">'. $mission .'</div>'; endif; ?>
-              <?php if ($tabs): print '<div id="tabs-wrapper" class="clear-block">'; endif; ?>
-              <?php if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif; ?>
-              <?php if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif; ?>
-              <?php if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif; ?>
-              <?php if ($show_messages && $messages): print $messages; endif; ?>
-              <?php if ($help): print $help; endif; ?>
-              <?php print $content; ?>
-		  </div>
-		
-		  <div id="sidebar-right">
-			<?php print $sidebar_right; ?>
-		  </div>
-		
-	    </div>
-	    <div id="footer">
-		  <div id="sharing">
-			  <a class="a2a_dd" href="http://www.addtoany.com/share_save?linkname=Bythuongtran&amp;linkurl=bythuongtran.com">
-				<img src="/<?php print path_to_theme(); ?>/images/icon-addtoany.png" alt="Add to any" />
-			  </a>
-			  <script type="text/javascript">a2a_linkname="Bythuongtran";a2a_linkurl="bythuongtran.com";</script>
-			  <script type="text/javascript" src="http://static.addtoany.com/menu/page.js"></script>
-			  <p class="share">Share this website</p>
-              <p class="social">Follow us at:</p>
-	          <a href="http://www.facebook.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-facebook.png" alt="Facebook" /></a>
-	          <a href="http://twitter.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-twitter.png" alt="Twitter" /></a>
-	          <a href="http://blog.bythuongtran.com/"><img src="/<?php print path_to_theme(); ?>/images/icon-blogger.png" alt="Blogger" /></a>
-	          <a href="http://www.flickr.com/photos/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-flickr.png" alt="Flickr" /></a>
-		  </div>
-		  <div id="notice">
-            <?php if (isset($secondary_links)) : ?>
-              <?php print theme('links', $secondary_links, array('class' => 'secondary-links')) ?>
-            <?php endif; ?>
-            <p class="copyright">&copy; Bythongtran 2010. All rights reserved</p>
-          </div>
+<head>
+<?php print $head ?>
+<title><?php print $head_title ?></title>
+<?php
+print $styles;
+print $scripts;
+?>
+</head>
+<body>
+<div id="page">
+	<div id="container">
+		<div id="header">
+			<div id="language-switcher">
+				<?php print $languageswitcher; ?>
+			</div>
+			<h1><span></span><?php print $site_name; ?></h1>
+			<div class="navigation">
+				<?php if (isset($primary_links)): print theme('links', $primary_links, array('class' => 'primary-links')); endif; ?>
+			</div>		
+		</div>
+		<div id="content" class="clearfix">
+			<div id="sidebar-left">
+				<?php print $sidebar_left; ?>
+				&nbsp;
+			</div>
+			<div id="body">
+				<?php
+				if ($mission): print '<div id="mission">'. $mission .'</div>'; endif;
+				if ($tabs): print '<div id="tabs-wrapper" class="clear-block">'; endif;
+				if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif;
+				if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif;
+				if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif;
+				if ($show_messages && $messages): print $messages; endif;
+				if ($help): print $help; endif;
+				print $content;
+				?>
+			</div>
+			<div id="sidebar-right">
+				<?php print $sidebar_right; ?>
+			</div>
+		</div>
+		<div id="footer">
+			<div id="sharing">
+				<a class="a2a_dd" href="http://www.addtoany.com/share_save?linkname=Bythuongtran&amp;linkurl=bythuongtran.com">
+					<img src="/<?php print path_to_theme(); ?>/images/icon-addtoany.png" alt="Add to any" />
+				</a>
+				<script type="text/javascript">a2a_linkname="Bythuongtran";a2a_linkurl="bythuongtran.com";</script>
+				<script type="text/javascript" src="http://static.addtoany.com/menu/page.js"></script>
+				<p class="share">Share this website</p>
+				<p class="social">Follow us at:</p>
+				<a href="http://www.facebook.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-facebook.png" alt="Facebook" /></a>
+				<a href="http://twitter.com/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-twitter.png" alt="Twitter" /></a>
+				<a href="http://blog.bythuongtran.com/"><img src="/<?php print path_to_theme(); ?>/images/icon-blogger.png" alt="Blogger" /></a>
+				<a href="http://www.flickr.com/photos/bythuongtran"><img src="/<?php print path_to_theme(); ?>/images/icon-flickr.png" alt="Flickr" /></a>
+			</div>
+			<div id="notice">
+				<?php if (isset($secondary_links)): print theme('links', $secondary_links, array('class' => 'secondary-links')); endif; ?>
+				<p class="copyright">&copy; <span class="btt">by</span>thuongtran 2010. All rights reserved.</p>
+			</div>
 		</div> 
-	  </div>
-    </div>
-  </body>
+	</div>
+</div>
+</body>
 </html>
