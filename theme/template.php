00af1c787b7026edbf8df353680b9f7508930eba
Sebastiaan Deckers <cbas@pandion.be>
2010-09-22 19:57:49 +0800

    Removing dead code

--- theme/template.php
+++ theme/template.php
@@ -1,7 +1,1 @@
 <?php
-/*function bythuongtran_breadcrumb($breadcrumb) {
-  if (!empty($breadcrumb)) {
-    array_shift($breadcrumb); //gets rid of "Home"
-    return '<div class="breadcrumb">' . implode('&nbsp;&raquo;&nbsp;', $breadcrumb) . '</div>';
-  }
-}*/
