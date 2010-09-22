851fd512af5c28624b34f58bf0be0ee06a21fbbd
Sebastiaan Deckers <cbas@pandion.be>
2010-09-22 19:57:26 +0800

    Pretty maintenance page

--- theme/maintenance-page.tpl.php
+++ theme/maintenance-page.tpl.php
@@ -4,17 +4,75 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <title>Wedding Planning - Wedding Specialist - bythuongtran</title>
-</head>
-<body style="text-align: center;">
-<p>
-	<img style="border-width: 0;" src="coming-soon.png" width="1000" height="750" />
-</p>
+<link href='http://fonts.googleapis.com/css?family=Josefin+Sans+Std+Light&subset=latin' rel='stylesheet' type='text/css' />
+<style>
+html {
+	height: 100%;
+}
+body {
+	margin: 0;
+	padding: 0;
+	border-width: 0;
+	height: 100%;
+	background-color: white;
+	background-image: url(http://farm3.static.flickr.com/2715/4316723291_8a9830b3d2_b.jpg);
+	background-attachment: fixed;
+	background-position: center center;
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+#overlay {
+	background-color: rgba(255, 255, 255, 0.6);
+	width: 100%;
+	height: 100%;
+	overflow: auto;
+	margin: auto;
+}
+#notice {
+	background-color: white;
+	border: 8px solid #dddddd;
+	border-top-left-radius: 24px;
+	border-bottom-right-radius: 24px;
+	padding: 16px;
 
-<?php print $help; ?>
-<?php print $messages; ?>
-<!--
-<?php print $content ?>
--->
+	-webkit-box-shadow: rgba(0, 0, 0, 0.15) 8px 8px 5px;
+	-moz-box-shadow: rgba(0, 0, 0, 0.15) 8px 8px 5px;
+	-o-box-shadow: rgba(0, 0, 0, 0.15) 8px 8px 5px;
+	box-shadow: rgba(0, 0, 0, 0.15) 8px 8px 5px;
+
+	font-family: 'Josefin Sans Std Light';
+	font-size: 24pt;
+
+	width: 50%;
+	margin: auto;
+	margin-top: 50px;
+}
+#notice a {
+	color: inherit;
+	text-decoration: none;
+	font-weight: bold;
+}
+#notice a:hover {
+	text-decoration: underline;
+}
+#notice .logo .btt {
+	color: #c84f50;
+}
+</style>
+</head>
+<body>
+	<div id="overlay">
+		<div id="notice">
+			<p>Welcome to <a href="http://www.bythuongtran.com/" class="logo"><span class="btt">By</span>thuongtran</a>, the certified wedding specialist in Vietnam and Singapore.</p>
+			<p>Our website is currently under maintenance.</p>
+			<p>Please check back later or visit the <a href="http://blog.bythuongtran.com/">blog</a> instead.</p>
+			<p>- <a href="mailto:thuong.tran@bythuongtran.com">Thuong Tran</a></p>
+			<?php
+				$str = $help . $messages . $content;
+				if ($str): print "<hr />" . $str; endif;
+			?>
+		</div>
+	</div>
 <script type="text/javascript">var _gaq=[["_setAccount","UA-12337083-1"],["_trackPageview"]]</script><script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>
 </body>
 </html>
